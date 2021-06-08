84a411d6c3af6af36c0a899c28f5a3bb0de478c1
Gonsoo Moon <gonsoomoon@gmail.com>
2021-06-08 08:34:00 +0000

    update Horovod train code

--- cifar10/code/source/train_horovod.py
+++ cifar10/code/source/train_horovod.py
@@ -206,36 +206,6 @@ def train_horovod(args):
 
 
 
-#     model = model.to(device)
-
-#     criterion = nn.CrossEntropyLoss().to(device)
-#     optimizer = torch.optim.SGD(model.parameters(), lr=args.lr, momentum=args.momentum)
-
-#     for epoch in range(0, args.epochs):
-#         running_loss = 0.0
-#         for i, data in enumerate(train_loader):
-#             # get the inputs
-#             inputs, labels = data
-#             inputs, labels = inputs.to(device), labels.to(device)
-
-#             # zero the parameter gradients
-#             optimizer.zero_grad()
-
-#             # forward + backward + optimize
-#             outputs = model(inputs)
-#             loss = criterion(outputs, labels)
-#             loss.backward()
-#             optimizer.step()
-
-#             # print statistics
-#             running_loss += loss.item()
-#             if i % 2000 == 1999:  # print every 2000 mini-batches
-#                 print("[%d, %5d] loss: %.3f" % (epoch + 1, i + 1, running_loss / 2000))
-#                 running_loss = 0.0
-#                 #break # 2000 미니 배치만 실행
-#     print("Finished Training")
-#     return _save_model(model, args.model_dir)
-
 
 
 if __name__ == "__main__":
