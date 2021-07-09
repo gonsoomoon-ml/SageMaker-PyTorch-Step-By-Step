23d7de0aaaf989cdd91843027194ebe59a4184e4
GonsMoon <gonsoomoon@gmail.com>
2021-07-09 09:12:44 +0000

    edit sm-ddp

--- cifar10/code/source/train_ddp.py
+++ cifar10/code/source/train_ddp.py
@@ -1,23 +1,22 @@
 import argparse
 import logging
 import os
+import sys
+import json
 
 import torch
-import torch.distributed as dist
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
-import torch.nn.parallel
-import torch.optim
+
 import torch.utils.data
 import torch.utils.data.distributed
 import torchvision
-import torchvision.models
 import torchvision.transforms as transforms
-from model_def import Net
+from torch.optim.lr_scheduler import StepLR
 
-import sys
-import json
+# 사용자 정의 모델
+from model_def import Net
 
 
 def _get_logger():
@@ -33,49 +32,13 @@ def _get_logger():
         l.handler_set = True
     return l  
 
+##############
+# 전역 변수
+##############
 logger = _get_logger()
-
-
 classes = ("plane", "car", "bird", "cat", "deer", "dog", "frog", "horse", "ship", "truck")
 
-def _metric_average(val, name):
-    tensor = torch.tensor(val)
-    avg_tensor = hvd.allreduce(tensor, name=name)
-    return avg_tensor.item()
-
-
-def test(model, test_loader):
-    model.eval()
-    test_loss = 0
-    test_accuracy = 0
-    with torch.no_grad():
-        for data, target in test_loader:
-            data, target = data.cuda(), target.cuda()
-            output = model(data)
-            test_loss += F.nll_loss(output, target, size_average=False).item()  # sum up batch loss
-            pred = output.max(1, keepdim=True)[1]  # get the index of the max log-probability
-            test_accuracy += pred.eq(target.view_as(pred)).sum().item()
-
-    # Horovod: use test_sampler to determine the number of examples in this worker's partition.
-    test_loss /= len(test_loader.sampler)
-    test_accuracy /= len(test_loader.sampler)
-
-    # Horovod: average metric values across workers.
-    test_loss = _metric_average(test_loss, "avg_loss")
-    test_accuracy = _metric_average(test_accuracy, "avg_accuracy")
-
-    logger.info(
-        "Test set: Average loss: {:.4f}, Accuracy: {:.2f}%\n".format(test_loss, 100 * test_accuracy)
-    )
-
-
-def _save_model(model, model_dir):
-    logger.info("Saving the model.")
-    path = os.path.join(model_dir, "model.pth")
-    # recommended way from http://pytorch.org/docs/master/notes/serialization.html
-    torch.save(model.cpu().state_dict(), path)
-
-
+    
 def _get_train_data_loader(batch_size, training_dir, **kwargs):
     logger.info("Get train data sampler and data loader")
     dataset = torchvision.datasets.CIFAR10(
@@ -87,16 +50,15 @@ def _get_train_data_loader(batch_size, training_dir, **kwargs):
         ),        
     )
 
-
     ######################    
     # DDP 코드
-    # SageMaker data parallel: Set num_replicas and rank in DistributedSampler
+    # SageMaker data parallel: Set num_replicas and rank in DistributedSampler
     ######################
+        
     train_sampler = torch.utils.data.distributed.DistributedSampler(
         dataset, num_replicas=dist.get_world_size(), rank= dist.get_rank()
     )
     
-
 
     train_loader = torch.utils.data.DataLoader(
         dataset, batch_size=batch_size, sampler=train_sampler, **kwargs
@@ -118,14 +80,13 @@ def _get_test_data_loader(test_batch_size, training_dir, **kwargs):
     )
     
     ######################    
-    # DDP 코드    
-    # SageMaker data parallel: Set num_replicas and rank in DistributedSampler    
+    # DDP 코드   
+    # SageMaker data parallel: Set num_replicas and rank in DistributedSampler    
     ######################        
     
     test_sampler = torch.utils.data.distributed.DistributedSampler(
         dataset, num_replicas=dist.get_world_size(), rank= dist.get_rank()
     )
-
 
     
     test_loader = torch.utils.data.DataLoader(
@@ -138,43 +99,42 @@ def _get_test_data_loader(test_batch_size, training_dir, **kwargs):
 # DDP 코드: 1. 라이브러리 임포트
 ######################    
 
-# SageMaker data parallel: Import the library PyTorch API
-import smdistributed.dataparallel.torch.distributed as dist
+import smdistributed.dataparallel.torch.distributed as dist
+from smdistributed.dataparallel.torch.parallel.distributed import DistributedDataParallel as DDP
 
-# SageMaker data parallel: Import the library PyTorch DDP
-from smdistributed.dataparallel.torch.parallel.distributed import DistributedDataParallel as DDP
-
-# SageMaker data parallel: Initialize the library
 dist.init_process_group()
 
-
 def main(args):
-    
-    ##  Horovod: initialize library ##
-    # hvd.init()
-    
-    batch_size = args.batch_size    
-    
+
     ######################    
     # DDP 코드 : 2. 배치 사이즈 결정       
-    # SageMaker data parallel: Scale batch size by world size
+    # SageMaker data parallel: Scale batch size by world size
     ######################        
-    batch_size //= dist.get_world_size()
-    batch_size = max(batch_size, 1)
-
+    
+    batch_size = args.batch_size        
+    batch_size //= dist.get_world_size()
+    batch_size = max(batch_size, 1)
+    if dist.get_rank() == 0:
+        logger.info("################################")            
+        logger.info(f"Global batch size: {args.batch_size}")            
+    logger.info(f"batch_size: {batch_size}")
+        
+    
     ######################    
     # DDP 코드 : 3. 각 GPU 를 DDP LIb 프로세스에 할당      
-    # SageMaker data parallel: Pin each GPU to a single library process.
+    # SageMaker data parallel: Pin each GPU to a single library process.
     ######################        
 
     local_rank = dist.get_local_rank()    
-    torch.cuda.set_device(local_rank)    
+    torch.cuda.set_device(local_rank) 
+    logger.info(f"local_rank: {local_rank}")
+
 #    model.cuda(local_rank)    
     
     ######################    
     # DDP 코드 : 4. 데이타 샘플러에 num_replicas, rank 정보 설정
     # 관련 코드는 아래 _get_train_data_loader() 정의에 있음
-    # SageMaker data parallel: Set num_replicas and rank in DistributedSampler
+    # SageMaker data parallel: Set num_replicas and rank in DistributedSampler
     ######################        
 
     kwargs = {"num_workers": 1, "pin_memory": True}
@@ -198,34 +158,36 @@ def main(args):
         )
     )
 
-    
     torch.manual_seed(args.seed)
     device = torch.device("cuda")
     
     ######################    
     # DDP 코드 : 5. 모델을 DDP 로 감싸기 
-    # SageMaker data parallel: Wrap the PyTorch model with the library's DDP
+    # SageMaker data parallel: Wrap the PyTorch model with the library's DDP
     ######################        
     
     model = DDP(Net().to(device))
     logger.info("Model loaded")    
-    torch.cuda.set_device(local_rank)
     model.cuda(local_rank)
+    
+    # 학습 준비 및 실행
 
     optimizer = optim.Adadelta(model.parameters(), lr=args.lr)
     scheduler = StepLR(optimizer, step_size=1, gamma=args.gamma)
     for epoch in range(1, args.epochs + 1):
         train(args, model, device, train_loader, optimizer, epoch)
-        if rank == 0:
+        if dist.get_rank() == 0:
             test(model, device, test_loader)
         scheduler.step()
-
-    if args.save_model:
-        torch.save(model.state_dict(), "cifar10_cnn.pt")
-
     
     logger.info("Training is finished")
-    return _save_model(model, args.model_dir)
+    
+    # 모델 저장
+    
+    if dist.get_rank() == 0:
+        _save_model(model, args.model_dir)
+        
+    return None
 
 
 def train(args, model, device, train_loader, optimizer, epoch):
@@ -237,18 +199,18 @@ def train(args, model, device, train_loader, optimizer, epoch):
         loss = F.nll_loss(output, target)
         loss.backward()
         optimizer.step()
-        if batch_idx % args.log_interval == 0 and args.rank == 0:
+        if batch_idx % args.log_interval == 0 and dist.get_rank() == 0:
             print(
                 "Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}".format(
                     epoch,
-                    batch_idx * len(data) * args.world_size,
+                    batch_idx * len(data) * dist.get_world_size(),
                     len(train_loader.dataset),
                     100.0 * batch_idx / len(train_loader),
                     loss.item(),
                 )
             )
         if args.verbose:
-            print("Batch", batch_idx, "from rank", args.rank)
+            print("Batch", batch_idx, "from rank", dist.get_rank())
 
 
 def test(model, device, test_loader):
@@ -271,8 +233,19 @@ def test(model, device, test_loader):
         )
     )
 
-            
+def _metric_average(val, name):
+    tensor = torch.tensor(val)
+    avg_tensor = hvd.allreduce(tensor, name=name)
+    return avg_tensor.item()
 
+
+def _save_model(model, model_dir):
+    logger.info("Saving the model.")
+    path = os.path.join(model_dir, "model.pth")
+    # recommended way from http://pytorch.org/docs/master/notes/serialization.html
+    torch.save(model.state_dict(), path)    
+
+    
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -312,26 +285,30 @@ if __name__ == "__main__":
         metavar="LR",
         help="initial learning rate (default: 0.001)",
     )
-
-
     parser.add_argument(
-        "--momentum", type=float, default=0.5, metavar="M", help="SGD momentum (default: 0.5)"
+        "--gamma",
+        type=float,
+        default=0.7,
+        metavar="M",
+        help="Learning rate step gamma (default: 0.7)",
     )
+    parser.add_argument(
+        "--verbose",
+        action="store_true",
+        default=False,
+        help="For displaying smdistributed.dataparallel-specific logs",
+    )
+
     parser.add_argument(
         "--seed", type=int, default=42, metavar="S", help="random seed (default: 42)"
     )
+    
     parser.add_argument(
         "--log-interval",
         type=int,
         default=10,
         metavar="N",
         help="how many batches to wait before logging training status",
-    )
-    parser.add_argument(
-        "--backend",
-        type=str,
-        default="gloo",
-        help="backend for distributed training (tcp, gloo on cpu and gloo, nccl on gpu)",
     )
 
 
@@ -359,26 +336,6 @@ if __name__ == "__main__":
     # parse arguments
     args = parser.parse_args() 
     
-    main(args)
     
-##########################    
-
-
-
-class Net(nn.Module):
-    ...
-    # Define model
-
-def train(...):
-    ...
-    # Model training
-
-def test(...):
-    ...
-    # Model evaluation
-
-def main():
-    
-
 if __name__ == '__main__':
-    main()    
+    main(args)    
