5ad6155fa4f4e9e99dcd377170d858a07ec7f5ff
GonsooMoon <gonsoomoon@gmail.com>
2021-07-29 06:02:16 +0000

    edit inference part

--- cifar10/code/source/inference.py
+++ cifar10/code/source/inference.py
@@ -46,25 +46,25 @@ logger = _get_logger()
 import logging
 def model_fn(model_dir):
     logger.info("--> model_dir : {}".format(model_dir))
+    
+    # 디바이스 할당
     device = "cuda" if torch.cuda.is_available() else "cpu"
     print("device: ", device)
     
+    # 모델 네트워크 로딩
     model = Net()
     
     logger.info("--> model network is loaded")    
 
-#     if torch.cuda.device_count() > 1:
-#         logger.info("Gpu count: {}".format(torch.cuda.device_count()))
-#         model = nn.DataParallel(model)
-
-    # 디폴트로 DataParallel 를 기술함.
-    # 이유는 weight 이름 앞에 module 을 붙이기 위해서 임
+    # 모델을 파이토치 DataParallel 의 모델로 변경
     model = nn.DataParallel(model)        
         
+    # 모델 아티펙트 경로       
     model_file_path = os.path.join(model_dir, "model.pth")
     print("model_file_path: ", model_file_path)                      
         
-        
+
+    # 모델 가중치 로딩    
     try:    
         with open(model_file_path, "rb") as f:
             model.load_state_dict(torch.load(f))
@@ -85,9 +85,11 @@ def input_fn(input_data, content_type):
     '''
     logger.info("#### input_fn starting ######")
     logger.info(f"content_type: {content_type}")    
+    
+    # 디바이스 할당
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
                 
-    
+    # 입력 데이타를 numpy array 로 변경
     if isinstance(input_data, (np.ndarray)):
         np_array = input_data # 로컬에서 테스트시에 numpy.ndarray 로 제공 되는 경우
     else: # 토치 서브를 통해서 numpy.ndarray가 제공되면, IOByte( 확인 필요) 로 제공되기에 디코딩이 필요함.
@@ -96,7 +98,7 @@ def input_fn(input_data, content_type):
     
     logger.info(f"np_array shape: {np_array.shape}  ")
     
-    
+    # np.array 를 토치 텐서로 변환
     tensor = torch.FloatTensor(
             np_array) if content_type in content_types.UTF8_TYPES else torch.from_numpy(np_array)
     
@@ -106,10 +108,16 @@ def input_fn(input_data, content_type):
 
 
 def predict_fn(data, model):
+    '''
+    모델의 추론 함수
+    '''
     logger.info("#### predict_fn starting ######")    
+    # 디바이스 할당
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     model.to(device)
     input_data = data.to(device)
+    
+    # 모델 추론
     model.eval()
     with torch.no_grad():
         output = model(input_data)
